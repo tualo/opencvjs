02a4c3e0a65fc25722a8d51cf1a6f9f9bfb35d64
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-02-15 14:36:41 +0100

    initial commit

---
+++ src/Middlewares/Middleware.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tualo\Office\OpenCVJS\Middlewares;
+use Tualo\Office\Basic\TualoApplication;
+use Tualo\Office\Basic\IMiddleware;
+
+class Middleware implements IMiddleware{
+    public static function register(){
+        TualoApplication::use('opencvjs',function(){
+            try{
+                TualoApplication::javascript('opencvjs', './opencvjs/lib/opencv.js',[],-10000);
+            }catch(\Exception $e){
+                TualoApplication::set('maintanceMode','on');
+                TualoApplication::addError($e->getMessage());
+            }
+        },-100); // should be one of the last
+    }
+}
---
+++ src/js/both/Loader.js
@@ -0,0 +1,4 @@
+Ext.define('Tualo.opencvjs.Loader', {
+    // needed to include this file
+});
+Ext.Loader.setPath('Tualo.opencvjs', './opencvjs');
